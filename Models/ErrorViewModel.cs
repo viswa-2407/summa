fe7771eed3ca260288b2d3a0c001365b8920ea2b
viswa-2407 <cviswa2407@gmail.com>
2024-04-29 15:46:34 +0530

    msg

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practice_git.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
